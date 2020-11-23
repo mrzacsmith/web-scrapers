57770ed1c484e2211bdd62f45c80d620889865c1
Zac Smith <zrs3141592@gmail.com>
2020-11-22 22:08:45 -0700

    handle pagination
    app is currently working, next to test other apis

--- job-board/index.js
+++ job-board/index.js
@@ -1,16 +1,45 @@
 const Sheet = require('./sheet.js')
 const fetch = require('node-fetch')
+require('colors')
 
-const run = async () => {
-  const sheet = new Sheet()
-  await sheet.load()
-
+const scrapePage = async (i) => {
   const res = await fetch(
-    `https://jobs.github.com/positions.json?description=javascript&location=remote`
+    `https://jobs.github.com/positions.json?page=${i}&search=react`
   )
   const json = await res.json()
 
-  console.log({ json })
+  const rows = json.map((job) => {
+    return {
+      company: job.company,
+      title: job.title,
+      location: job.location,
+      date: job.created_at,
+      url: job.url,
+    }
+  })
+
+  return rows
+}
+
+const run = async () => {
+  let i = 1
+  let rows = []
+  while (true) {
+    const newRows = await scrapePage(i)
+    if (newRows.length === 0) break
+    console.log(i, 'new row length'.america, newRows.length)
+
+    rows = [...rows, ...newRows]
+    i++
+  }
+
+  console.log('total rows length'.america, rows.length)
+
+  const sheet = new Sheet()
+  await sheet.load()
+
+  await sheet.addRows(rows)
+  console.log('\n** all done **'.america)
 }
 
 run()
